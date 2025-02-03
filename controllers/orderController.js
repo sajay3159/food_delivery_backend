a1a5803d474f1f478c5b70450ddca254053f8d9b
Ajay Sahani <ajay.sahani@gmail.com>
2025-02-03 14:55:03 +0530

    work on Order Page

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -79,4 +79,15 @@ const verifyOrder = async (req, res) => {
 
 }
 
-export { placeOrder, verifyOrder };
+// user orders for frontend
+const userOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({ userId: req.body.userId })
+        res.json({ success: true, data: orders })
+    } catch (error) {
+        console.error(error)
+        res.json({ success: false, message: "Error" })
+    }
+}
+
+export { placeOrder, verifyOrder, userOrders };
