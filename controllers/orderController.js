54f20b55cec24a8fa0b1db575c8c9cbcc8156e51
Ajay Sahani <115401286+sajay3159@users.noreply.github.com>
2025-02-13 09:58:29 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-const frontend_url = "https://food-delivery-frontend-2pnl.onrender.com"
+const frontend_url = "https://food-delivery-frontend-zkwb.onrender.com"
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
